471f090f9fb01a76df225b444ac8e68bfeb353cd
Alice Lunardi <124783296+alicelunaaardi@users.noreply.github.com>
2024-06-24 10:16:05 -0300

    Add files via upload

---
+++ aulaRoom/app/src/main/java/com/example/aularoom/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.aularoom
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.aularoom.model.UsuarioModel
+import com.example.aularoom.repository.UsuarioDataBase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //Instancia de Room
+        val usuarioDatabase = UsuarioDataBase.getDataBase(this).usuarioDAO()//Aqui estamos instanciando a classe do Room, ao mesmo tempo que estamos retornando a classe de usuarioDao
+
+        usuarioDatabase.deleteUser(UsuarioModel().apply {
+            this.id_usuario = 4
+            //this.nome = "Sarah Ferraz"
+            //this.idade = 98
+
+        })
+
+        val retornoSelectMultiplo = usuarioDatabase.getAll()//Retorna todos os registros
+
+        for(item in retornoSelectMultiplo){
+            Log.d("Retorno Múltiplo", "id_usuario: ${item.id_usuario}, nome: ${item.nome}, idade: ${item.idade}")
+        }
+
+
+    }
+}
